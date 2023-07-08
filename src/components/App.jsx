f0521d2b37bd2112ec9bde88ce94248d497541d1
Nikita_Beliy <beliynikita1995@gmail.com>
2023-07-08 15:01:05 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,28 @@
-import { Component } from "react";
+import { Component } from 'react';
 
-import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
-import { Statistics } from "./Statistics/Statistics";
-import { Notification } from "./Notification/Notification";
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
 
 export class App extends Component {
-
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
-
-  handleOptionClick = (option) => {
-
-    this.setState((prevState) => {
+  handleOptionClick = option => {
+    this.setState(prevState => {
       return {
         [option]: prevState[option] + 1,
-      }
-    })
-  }
+      };
+    });
+  };
 
   render() {
-    const { good, neutral, bad } = this.state
+    const { good, neutral, bad } = this.state;
     const totalFeedback = good + neutral + bad;
-    const positiveFeedback = Math.round(good / totalFeedback * 100)
+    const positiveFeedback = Math.round((good / totalFeedback) * 100);
 
     return (
       <div
@@ -33,17 +30,28 @@ export class App extends Component {
           height: '100vh',
           display: 'flex',
           justifyContent: 'center',
-          flexDirection: "column",
+          flexDirection: 'column',
           alignItems: 'center',
           fontSize: 40,
-          color: '#010101'
+          color: '#010101',
         }}
       >
         <FeedbackOptions
           handleOptionClick={this.handleOptionClick}
-          options={this.state} />
-        {totalFeedback ? <Statistics good={good} neutral={neutral} bad={bad} total={totalFeedback} positiveFeedback={positiveFeedback}></Statistics> : <Notification message="There is no feedback" />}
+          options={this.state}
+        />
+        {totalFeedback ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positiveFeedback={positiveFeedback}
+          ></Statistics>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
       </div>
     );
   }
-};
+}
